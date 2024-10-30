dd3f68722b212c92bef5bf9fb07c87c8beeb9209
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-30 11:12:27 +0100

    Modification route pour le composant Article

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter,  RouterProvider } from "react-router-dom";
 
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-
+import Article from "./pages/Article";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <main>
-          <Home />
-        </main>
-      </>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <main>
-          <About />
-        </main>
-      </>
-    ),
-  },
+    path: "/articles/id",
+    element:<Article/>,
+},
 ]);
 
 const rootElement = document.getElementById("root");
---
+++ src/pages/Article.tsx
@@ -0,0 +1,6 @@
+function Article() {
+    return <h1>Hello from Article</h1>;
+  }
+  
+  export default Article;
+  
